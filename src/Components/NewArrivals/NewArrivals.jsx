4a748c5b38ec0926f45fad7b2af186f9d4d7c4e5
jmathtech <jamil.matheny@gmail.com>
2022-07-30 14:33:40 -0400

    Added cards to NewArrivals.jsx and updated index.html

--- src/Components/NewArrivals/NewArrivals.jsx
+++ src/Components/NewArrivals/NewArrivals.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
-import "./NewArrivals.css"
+import React from "react";
+import "./NewArrivals.css";
 
 const NewArrivals = () => {
   return (
     <div class="midtier">
-        <div class="container">
-          <h1 class="text-center">NEW ARRIVAL ACCESSORIES</h1>
-          <div class="box-header">
-
+      <div class="container">
+        <h1 class="text-center">NEW ARRIVAL ACCESSORIES</h1>
+        <div class="box-header">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m4">
+                <div class="card">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img
+                      class="activator"
+                      src="../../img/suits/001/suits_001.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">
+                      Card Title<i class="material-icons right">more_vert</i>
+                    </span>
+                    <p>This is a link</p>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">
+                      Card Title<i class="material-icons right">close</i>
+                    </span>
+                    <p>
+                      Here is some more information about this product that is
+                      only revealed once clicked on.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default NewArrivals
+export default NewArrivals;
