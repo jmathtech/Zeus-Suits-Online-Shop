77f5287e965a043c1932c29198d7f3291f18df7d
jmathtech <jamil.matheny@gmail.com>
2022-07-31 18:49:30 -0400

    Updated css files to format text header alignment

--- src/Components/NewArrivals/NewArrivals.jsx
+++ src/Components/NewArrivals/NewArrivals.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import "./NewArrivals.css";
+import Image from "../../img/suits/001/suits_001.jpg";
+import Image2 from "../../img/suits/002/suits_001.jpg";
+import Image3 from "../../img/suits/003/suits_001.jpg";
+import Image4 from "../../img/suits/004/suits_001.jpg";
+import Image5 from "../../img/suits/005/suits_001.jpg";
+import Image6 from "../../img/suits/006/suits_001.jpg";
+import Image7 from "../../img/suits/007/suits_001.jpg";
+import Image8 from "../../img/suits/008/suits_001.jpg";
 
-const NewArrivals = () => {
+export default function NewArrivals() {
   return (
-    <div class="midtier">
-      <div class="container">
-        <h1 class="text-center">NEW ARRIVAL ACCESSORIES</h1>
-        <div class="box-header">
-          <div class="container">
+    <>
+      <div class="midtier">
+        <div class="container">
+          <h1 class="text-center">NEW ARRIVAL ACCESSORIES</h1>
+          <div class="box-header">
             <div class="row">
-              <div class="col s12 m4">
+              <div class="col s12 m4 l3">
                 <div class="card large">
                   <div class="card-image waves-effect waves-block waves-light">
-                    <img
-                      class="activator"
-                      src="../../img/suits/001/suits_001.jpg"
-                      alt=""
-                    />
+                    <img class="activator" src={Image} alt="" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
@@ -37,14 +41,10 @@ const NewArrivals = () => {
                 </div>
               </div>
 
-              <div class="col s12 m4">
+              <div class="col s12 m4 l3">
                 <div class="card large">
                   <div class="card-image waves-effect waves-block waves-light">
-                    <img
-                      class="activator"
-                      src="../../img/suits/002/suits_002.jpg"
-                      alt=""
-                    />
+                    <img class="activator" src={Image2} alt="" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
@@ -65,20 +65,16 @@ const NewArrivals = () => {
                 </div>
               </div>
 
-              <div class="col s12 m4">
+              <div class="col s12 m4 l3">
                 <div class="card large">
                   <div class="card-image waves-effect waves-block waves-light">
-                    <img
-                      class="activator"
-                      src="../../img/suits/003/suits_003.jpg"
-                      alt=""
-                    />
+                    <img class="activator" src={Image3} alt="" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
                       Suit 03<i class="material-icons right">more_vert</i>
                     </span>
-                    <p>Price: $10999.99</p>
+                    <p>Price: $9999.99</p>
                   </div>
                   <div class="card-reveal">
                     <span class="card-title grey-text text-darken-4">
@@ -88,19 +84,15 @@ const NewArrivals = () => {
                       Here is some more information about this product that is
                       only revealed once clicked on.
                     </p>
-                    <p>Price: $10999.99</p>
+                    <p>Price: $9999.99</p>
                   </div>
                 </div>
               </div>
 
-              <div class="col s12 m4">
+              <div class="col s12 m4 l3">
                 <div class="card large">
                   <div class="card-image waves-effect waves-block waves-light">
-                    <img
-                      class="activator"
-                      src="../../img/suits/004/suits_004.jpg"
-                      alt=""
-                    />
+                    <img class="activator" src={Image4} alt="" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
@@ -121,14 +113,10 @@ const NewArrivals = () => {
                 </div>
               </div>
 
-              <div class="col s12 m4">
+              <div class="col s12 m4 l3">
                 <div class="card large">
                   <div class="card-image waves-effect waves-block waves-light">
-                    <img
-                      class="activator"
-                      src="../../img/suits/004/suits_004.jpg"
-                      alt=""
-                    />
+                    <img class="activator" src={Image5} alt="" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
@@ -149,14 +137,10 @@ const NewArrivals = () => {
                 </div>
               </div>
 
-              <div class="col s12 m4">
+              <div class="col s12 m4 l3">
                 <div class="card large">
                   <div class="card-image waves-effect waves-block waves-light">
-                    <img
-                      class="activator"
-                      src="../../img/suits/004/suits_004.jpg"
-                      alt=""
-                    />
+                    <img class="activator" src={Image6} alt="" />
                   </div>
                   <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">
@@ -177,12 +161,57 @@ const NewArrivals = () => {
                 </div>
               </div>
 
+              <div class="col s12 m4 l3">
+                <div class="card large">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator" src={Image7} alt="" />
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">
+                      Suit 07<i class="material-icons right">more_vert</i>
+                    </span>
+                    <p>Price: $4999.99</p>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">
+                      Suit 07<i class="material-icons right">close</i>
+                    </span>
+                    <p>
+                      Here is some more information about this product that is
+                      only revealed once clicked on.
+                    </p>
+                    <p>Price: $4999.99</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col s12 m4 l3">
+                <div class="card large">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator" src={Image8} alt="" />
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">
+                      Suit 08<i class="material-icons right">more_vert</i>
+                    </span>
+                    <p>Price: $6499.99</p>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">
+                      Suit 08<i class="material-icons right">close</i>
+                    </span>
+                    <p>
+                      Here is some more information about this product that is
+                      only revealed once clicked on.
+                    </p>
+                    <p>Price: $6499.99</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default NewArrivals;
